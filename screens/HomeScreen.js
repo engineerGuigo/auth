58fa8f0527db684beb89b49a5cb802cd4a18da8a
Rodrigo Durante <rodrigo.durantee@hotmail.com>
2020-06-29 18:19:19 -0500

    homescreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,32 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import * as firebase from "firebase";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 export default class HomeScreen extends React.Component {
+  state = {
+    email: "",
+    displayName: "",
+  };
+
+  componentDidMount() {
+    const { email, displayName } = firebase.auth().currentUser;
+
+    this.setState({ email, displayName });
+  }
+
+  signOutUser = () => {
+    firebase.auth().signOut();
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Home Screen</Text>
+        <Text>Hi {this.state.email}!</Text>
+
+        <TouchableOpacity style={{ marginTop: 32 }} onPress={this.signOutUser}>
+          <Text>Logout</Text>
+        </TouchableOpacity>
       </View>
     );
   }
